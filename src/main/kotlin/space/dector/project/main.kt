46d2c8862e8141a8cbb32d2db1e760d1193bb249
Denys M <dector@dector.space>
2021-06-06 18:18:09 +0400

    Add JWT check for accessing content

--- src/main/kotlin/space/dector/project/main.kt
+++ src/main/kotlin/space/dector/project/main.kt
@@ -1,9 +1,14 @@
 package space.dector.project
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.core.Uri
+import org.http4k.core.cookie.Cookie
+import org.http4k.core.cookie.SameSite
+import org.http4k.core.cookie.cookie
 import org.http4k.server.Http4kServer
 import org.http4k.server.Netty
 import org.http4k.server.asServer
@@ -17,24 +22,36 @@ import kotlin.io.path.isDirectory
 
 
 fun main() {
-    val port = System.getProperty("GATEKEEPER_PORT")
+    val port = System.getenv("GATEKEEPER_PORT")
         ?.toIntOrNull()
         ?: 9090
-    val servingFolder = (System.getProperty("GATEKEEPER_FOLDER")
+    val servingFolder = (System.getenv("GATEKEEPER_FOLDER")
         ?: "public")
         .let(::Path)
+    val jwtSecret = System.getenv("GATEKEEPER_JWT_SECRET")
+        ?: error("JWT secret is not defined: GATEKEEPER_JWT_SECRET not found.")
+
+    val accessManager = AccessManager(
+        jwtSecret = jwtSecret,
+    )
 
     val config = Netty(port = port)
     val server = { request: Request ->
-        val interceptionResult = interceptRequest(request)
+        val interceptionResult = interceptRequest(request, accessManager)
 
         when (interceptionResult) {
             InterceptionResult.NotAuthorized -> {
                 Response(Status.TEMPORARY_REDIRECT)
                     .header("Location", "/auth/login")
             }
-            InterceptionResult.Ok -> {
+            InterceptionResult.Ok.ForContent -> {
                 respondWithFileContent(request.uri, servingFolder)
+            }
+            is InterceptionResult.Ok.ForService -> {
+                when (interceptionResult.page) {
+                    ServicePage.Login ->
+                        respondWithLoginPage(request, accessManager)
+                }
             }
         }
     }.asServer(config).start()
@@ -45,13 +62,21 @@ fun main() {
     server.block()
 }
 
-private fun interceptRequest(request: Request): InterceptionResult {
-    if (request.uri.toString() == ServicePages.Login)
-        return InterceptionResult.Ok
+private fun interceptRequest(
+    request: Request,
+    accessManager: AccessManager,
+): InterceptionResult {
+    val servicePage = ServicePage.forPath(request.uri.path)
+    if (servicePage != null)
+        return InterceptionResult.Ok.ForService(servicePage)
 
-    return InterceptionResult.Ok
-    // TODO Validate user JWT
-//    return InterceptionResult.NotAuthorized
+    val token = request.cookie("SERVICE_TOKEN")
+        ?.value
+        ?: return InterceptionResult.NotAuthorized
+
+    return if (accessManager.verify(token))
+        InterceptionResult.Ok.ForContent
+    else InterceptionResult.NotAuthorized
 }
 
 private fun respondWithFileContent(uri: Uri, servingFolder: Path): Response {
@@ -73,11 +98,21 @@ private fun respondWithFileContent(uri: Uri, servingFolder: Path): Response {
 
 private sealed class InterceptionResult {
     object NotAuthorized : InterceptionResult()
-    object Ok : InterceptionResult()
+    sealed class Ok : InterceptionResult() {
+        object ForContent : Ok()
+        data class ForService(val page: ServicePage) : Ok()
+    }
 }
 
-private object ServicePages {
-    const val Login = "/auth/login"
+private enum class ServicePage(val path: String) {
+    Login("/auth/login"),
+    ;
+
+    companion object {
+        private val allPages = values()
+
+        fun forPath(path: String): ServicePage? = allPages.firstOrNull { it.path == path }
+    }
 }
 
 private fun Http4kServer.addShutdownHook() {
@@ -97,4 +132,85 @@ private fun Path.contentTypeOrDefault(): String {
             .getOrNull()
             ?: "text/plain"
     }
+}
+
+private class AccessManager(
+    jwtSecret: String,
+) {
+
+    private val jwtTools = JwtTools(jwtSecret)
+
+    private val allowedUsers = listOf<String>(
+        "dan@example.com",
+    )
+
+    fun verify(token: String): Boolean {
+        val userEmail = jwtTools.parseUserEmail(token)
+            ?: return false
+
+        return userEmail in allowedUsers
+    }
+
+    fun createTokenFor(email: String): String? {
+        return jwtTools.createToken(email)
+    }
+}
+
+class JwtTools(
+    secret: String,
+) {
+    private val issuer = "auth0"
+
+    private val algorithm: Algorithm = Algorithm.HMAC256(secret)
+    private val verifier = JWT.require(algorithm)
+        .withIssuer(issuer)
+        .build()
+
+    fun createToken(email: String): String? =
+        runCatching {
+            JWT.create()
+                .withIssuer(issuer)
+                .withSubject(email)
+                .sign(algorithm)
+        }.getOrNull()
+
+    fun parseUserEmail(token: String): String? {
+        val jwt = runCatching { verifier.verify(token) }
+            .getOrNull()
+
+        return jwt?.subject
+    }
+}
+
+private fun respondWithLoginPage(request: Request, accessManager: AccessManager): Response {
+    val tokenToSet = request.query("code")
+        ?.let { code -> getEmailForLoginCodeOrNull(code) }
+        ?.let { email -> accessManager.createTokenFor(email) }
+
+    return if (tokenToSet != null) {
+        Response(Status.TEMPORARY_REDIRECT)
+            .header("Location", "/")
+            .cookie(
+                Cookie(
+                    name = "SERVICE_TOKEN",
+                    value = tokenToSet,
+                    secure = true,
+                    httpOnly = true,
+                    path = "/",
+                )
+            )
+    } else {
+        Response(Status.OK)
+            .body("TODO: login")
+    }
+}
+
+// TODO use storage
+private fun getEmailForLoginCodeOrNull(code: String): String? {
+    // TODO fetch values from storage
+    val data = mapOf(
+        "1234" to "dan@example.com",
+    )
+
+    return data[code]
 }
